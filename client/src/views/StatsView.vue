56a3778064aee4ac6210f72ca6e17d1d4bec2583
Shel <shelbeysmithx@gmail.com>
2024-02-22 22:24:27 -0500

    vue scaffold

---
+++ client/src/views/StatsView.vue
@@ -0,0 +1,139 @@
+<template>
+<div class="columns is-centered">
+
+<div class="column is-half">
+
+  <div class="block">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          Today
+        </p>
+        <a href="#" class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="ri-corner-right-down-line"></i>
+          </span>
+        </a>
+      </header>
+      <div class="card-content">
+        <div class="content">
+
+          <div class="columns">
+            <div class="column">
+              <div class="block">
+                <h3 class="title is-5 center">DISTANCE</h3>
+                <div class="center">0.0</div>
+              </div>
+              <div class="block">
+                <h3 class="title is-5 center">TIME</h3>
+                <div class="center">0.0</div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="block">
+                <h3 class="title is-5 center">PACE</h3>
+                <div class="center">0.0</div>
+              </div>
+              <div class="block">
+                <h3 class="title is-5 center">CALORIES</h3>
+                <div class="center">0.0</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="block">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          This Week
+        </p>
+        <a href="#" class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="ri-corner-right-down-line"></i>
+          </span>
+        </a>
+      </header>
+      <div class="card-content">
+        <div class="content">
+
+          <div class="columns">
+            <div class="column">
+              <div class="block">
+                <h3 class="title is-5 center">DISTANCE</h3>
+                <div class="center">0.0</div>
+              </div>
+              <div class="block">
+                <h3 class="title is-5 center">TIME</h3>
+                <div class="center">0.0</div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="block">
+                <h3 class="title is-5 center">PACE</h3>
+                <div class="center">0.0</div>
+              </div>
+              <div class="block">
+                <h3 class="title is-5 center">CALORIES</h3>
+                <div class="center">0.0</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="block">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          This Year
+        </p>
+        <a href="#" class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="ri-corner-right-down-line"></i>
+          </span>
+        </a>
+      </header>
+      <div class="card-content">
+        <div class="content">
+
+          <div class="columns">
+            <div class="column">
+              <div class="block">
+                <h3 class="title is-5 center">DISTANCE</h3>
+                <div class="center">0.0</div>
+              </div>
+              <div class="block">
+                <h3 class="title is-5 center">TIME</h3>
+                <div class="center">0.0</div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="block">
+                <h3 class="title is-5 center">PACE</h3>
+                <div class="center">0.0</div>
+              </div>
+              <div class="block">
+                <h3 class="title is-5 center">CALORIES</h3>
+                <div class="center">0.0</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</div>
+
+</div>
+  </template>
