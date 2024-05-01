b23cf7240d0b9f3c2a22a61a7a922ea4aa0b21a7
Shelacula <shelacula@yahoo.com>
2024-05-01 01:44:26 -0400

    error fixing

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -11,6 +11,7 @@ const users = ref([] as User[]);
 const session = refSession();
 
 getUsers()
+        .then((data) => users.value = data.slice(0, 5))
         .catch((error) => console.error(error));
     ;
 
